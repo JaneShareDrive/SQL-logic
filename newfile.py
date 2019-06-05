168b72f343ca32061b98d3559982da6bfda1ceb0
Tarit <janesharedrive@gmail.com>
2019-06-04 20:23:49 -0400

    third_upload

--- newfile.py
+++ newfile.py
@@ -1,16 +1,21 @@
 import pyodbc
-import pandas
-from pandas import DataFrame
+import pandas as pd
 
 cnxn = pyodbc.connect(r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\users\bartogre\desktop\CorpRentalPivot1.accdb;UID="";PWD="";')
 crsr = cnxn.cursor()
 for table_name in crsr.tables(tableType='TABLE'):
     print(table_name)
 cursor = cnxn.cursor()
-sql = "Select sum(CYTM), sum(PYTM), BRAND From data Group By BRAND"
+
+#get topics
+sql = "Select distinct Topic_Code From student_practice"
+df = pd.read_sql(sql, cnxn)
+return df.to_json(orient='values')
 
 #get questions by topic
 sql = "Select question_ID, question From student_practice where Topic_Code is topic order by question_ID"
+df = pd.read_sql(sql, cnxn)
+return df.to_json(orient='records')
 
 #get answers by topic
 sql = "Select question, correct_answer, explanation From student_practice where Topic_Code is topic order by question_ID"
