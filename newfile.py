d6038e0ffa0d12c845d9c6b2f35c7b252067900d
Tarit <janesharedrive@gmail.com>
2019-06-05 15:38:59 -0400

    third_upload

--- newfile.py
+++ newfile.py
@@ -2,45 +2,39 @@ import pyodbc
 import pandas as pd
 import json
 
-cnxn = pyodbc.connect(r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\users\bartogre\desktop\CorpRentalPivot1.accdb;UID="";PWD="";')
-crsr = cnxn.cursor()
-for table_name in crsr.tables(tableType='TABLE'):
-    print(table_name)
-cursor = cnxn.cursor()
-
 #get topics
 def getTopics():
   cnxn = pyodbc.connect(sqldb_connect)
   sql = "Select distinct Topic_Code From student_practice"
   df = pd.read_sql(sql, cnxn)
-  return df.to_json(orient='values')
+  return df.to_json(orient='records')
   cnxn.close()
 
 #get questions by topic
 def getQuestionsT(topicIn):
   topic = json.loads(topicIn)
-  sql = "Select question_ID, question From student_practice where Topic_Code is topic order by question_ID"
+  sql = f"Select question_ID, question From student_practice where Topic_Code = '{topic}' order by question_ID"
   df = pd.read_sql(sql, cnxn)
   return df.to_json(orient='records')
 
 #get answers by topic
 def getAnswersT(topicIn):
   topic = json.loads(topicIn)
-  sql = "Select question, correct_answer, explanation From student_practice where Topic_Code is topic order by question_ID"
+  sql = f"Select question, correct_answer, explanation From student_practice where Topic_Code = '{topic}' order by question_ID"
   df = pd.read_sql(sql, cnxn)
   return df.to_json(orient='records')
 
 #get questions by difficulty
 def getQuestionsD(difficultyIn):
   difficulty = json.loads(difficultyIn)
-  sql = "Select question_ID, question From student_practice where Difficulty is difficulty order by question_ID"
+  sql = f"Select question_ID, question, possible_answer From student_practice where Difficulty is '{difficulty}' order by question_ID"
   df = pd.read_sql(sql, cnxn)
   return df.to_json(orient='records')
 
 #get answers by difficulty
 def getAnswersD(difficultyIn):
   difficulty = json.loads(difficultyIn)
-  sql = "Select question, correct_answer, explanation From student_practice where Difficulty is difficulty order by question_ID"
+  sql = f"Select question, correct_answer, explanation From student_practice where Difficulty is '{difficulty}' order by question_ID"
   df = pd.read_sql(sql, cnxn)
   return df.to_json(orient='records')
 
