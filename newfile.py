776feb45d3b4f279699cdef0b98b5d297ce5677f
Tarit <janesharedrive@gmail.com>
2019-06-05 20:24:22 -0400

    third_upload

--- newfile.py
+++ newfile.py
@@ -7,40 +7,57 @@ def getTopics():
   cnxn = pyodbc.connect(sqldb_connect)
   sql = "Select distinct Topic_Code From student_practice"
   df = pd.read_sql(sql, cnxn)
-  return df.to_json(orient='records')
   cnxn.close()
+  return df.to_json(orient='records')
 
 #get questions by topic
 def getQuestionsT(topicIn):
+  cnxn = pyodbc.connect(sqldb_connect)
   topic = json.loads(topicIn)
-  sql = f"Select question_ID, question From student_practice where Topic_Code = '{topic}' order by question_ID"
+  sql = f"Select question_ID, question, possible_answer From student_practice where Topic_Code = '{topic}' order by question_ID"
   df = pd.read_sql(sql, cnxn)
+  cnxn.close()
   return df.to_json(orient='records')
 
 #get answers by topic
 def getAnswersT(topicIn):
+  cnxn = pyodbc.connect(sqldb_connect)
   topic = json.loads(topicIn)
   sql = f"Select question, correct_answer, explanation From student_practice where Topic_Code = '{topic}' order by question_ID"
   df = pd.read_sql(sql, cnxn)
+  cnxn.close()
   return df.to_json(orient='records')
 
 #get questions by difficulty
 def getQuestionsD(difficultyIn):
+  cnxn = pyodbc.connect(sqldb_connect)
   difficulty = json.loads(difficultyIn)
   sql = f"Select question_ID, question, possible_answer From student_practice where Difficulty is '{difficulty}' order by question_ID"
   df = pd.read_sql(sql, cnxn)
+  cnxn.close()
   return df.to_json(orient='records')
 
 #get answers by difficulty
 def getAnswersD(difficultyIn):
+  cnxn = pyodbc.connect(sqldb_connect)
   difficulty = json.loads(difficultyIn)
   sql = f"Select question, correct_answer, explanation From student_practice where Difficulty is '{difficulty}' order by question_ID"
   df = pd.read_sql(sql, cnxn)
+  cnxn.close()
   return df.to_json(orient='records')
 
-#score tests. Takes 'submitted' parameter, which is a list of tuples: ([(subMittedanswer, correctAnswer)])
+#score tests. NOT WORKING
+def scoreTests(qaTuple):
+  i = 0
+  cnxn = pyodbc.connect(sqldb_connect)
+  sql = Select question_ID, correct_answer from student_practice
+  df = pd.read_sql(sql, cnxn)
+  cnxn.close()
+  QAs = json.loads(qaTuple) #dictionary
+  for key, value in QAs:
+    for 
+    if df[0].key = 
 
-i = 0
 for a1, a2 in submitted:
   if a1==a2:
       i+=1
